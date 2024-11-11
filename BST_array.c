d152c68ab89a89a3eb4b224a7dc46199802a1d10
NRG2005 <144699351+NRG2005@users.noreply.github.com>
2024-11-11 22:21:04 +0530

    BST_array.c

--- BST_array.c
+++ BST_array.c
@@ -1,39 +1,73 @@
-#include<stdio.h>
-#include<stdlib.h>
+#include <stdio.h>
 
-int child(int tree[])
+// storing the maximum number of nodes
+int max_node = 15;
+
+// array to store the tree
+char tree[] = {'\0', 'D', 'A', 'F', 'E', 'B', 'R', 'T', 'G', 'Q', '\0', '\0', 'V', '\0', 'J', 'L'};
+
+int get_right_child(int index) 
 {
-	int n;
-	int root;
-	char value;
-	char right_child;
-	char left_child;
-	printf("Enter the number of nodes in the tree : ");
-	scanf("%d",&n);
-	for(int i=0;i<=n;i++)
+  	// node is not null
+  	// and the result must lie within the number of nodes 
+	// for a complete binary tree
+  	if (tree[index] != '\0' && ((2 * index) + 1) <= max_node)
+    		return (2 * index) + 1;
+  	// right child doesn't exist
+  	return -1;
+}
+
+int get_left_child(int index) 
+{
+  	// node is not null
+  	// and the result must lie within the number of nodes 
+	// for a complete binary tree
+  	if (tree[index] != '\0' && (2 * index) <= max_node)
+    		return 2 * index;
+  	// left child doesn't exist
+  	return -1;
+}
+
+void preorder(int index) 
+{
+  	// checking for valid index and null node
+  	if (index > 0 && tree[index] != '\0') 
 	{
-		printf("Enter the value in the node : ");
-		scanf("%c",&value);
-		value = tree[i];
-	}
-	printf("Enter the index of the root node (0 or 1) : ");
-	scanf("%d",&root);
-	if(root<0 || root>1)
+    		printf(" %c ", tree[index]); // visiting root
+    		preorder(get_left_child(index)); 
+    		preorder(get_right_child(index));   			}
+}
+
+void postorder(int index) 
+{
+  	// checking for valid index and null node
+  	if (index > 0 && tree[index] != '\0') 
 	{
-		printf("The root is invalid\n");
-	}
-	else if(root == 0 || root == 1)
+    		postorder(get_left_child(index)); 
+    		postorder(get_right_child(index)); 
+    		printf(" %c ", tree[index]); //visiting root
+  	}
+}
+
+void inorder(int index) 
+{
+  	// checking for valid index and null node
+  	if (index > 0 && tree[index] != '\0') 
 	{
-		for(int i=0;i<=n;i++)
-		{
-			if(tree[i] != '\0' && ((2*i)+1) <= n)
-			{
-				right_child = tree[2*i];
-				left_child = tree[2*i]+1;
-			} 
-		}
-	}
-	printf("left child = %c\n",left_child);
-	printf("right child = %c\n",right_child);
-	return 0;
+    		inorder(get_left_child(index)); //visiting left subtree
+    		printf(" %c ", tree[index]); //visiting root
+    		inorder(get_right_child(index)); 
+  	}
+}
+
+int main() 
+{
+  	printf("Preorder:\n");
+  	preorder(1);
+  	printf("\nPostorder:\n");
+  	postorder(1);
+  	printf("\nInorder:\n");
+  	inorder(1);
+  	printf("\n");
+  	return 0;
 }
